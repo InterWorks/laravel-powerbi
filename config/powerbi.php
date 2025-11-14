15d7cffa8fea483c0a39873021a7608324d26daf
mrkbingham <mark.bingham@interworks.com>
2025-11-14 13:37:46 -0600

    adds config

--- config/powerbi.php
+++ config/powerbi.php
@@ -2,5 +2,7 @@
 
 // config for InterWorks/PowerBI
 return [
-
+    'tenant' => env('POWER_BI_TENANT', ''),
+    'client_id' => env('POWER_BI_CLIENT_ID', ''),
+    'client_secret' => env('POWER_BI_CLIENT_SECRET', ''),
 ];
