7efe7e7d81ae1d0024aa98ba4d6ecce48c3f8d5e
mrkbingham <mark.bingham@interworks.com>
2025-11-14 13:39:45 -0600

    adds get groups test

---
+++ tests/Unit/Requests/Groups/GetGroupsTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use InterWorks\PowerBI\DTO\Group;
+use InterWorks\PowerBI\DTO\Groups;
+use InterWorks\PowerBI\PowerBI;
+use InterWorks\PowerBI\Requests\Groups\GetGroups;
+use InterWorks\PowerBI\Tests\Fixtures\PowerBIFixture;
+use Saloon\Http\Faking\MockClient;
+
+test('can get groups', function () {
+    $mockClient = new MockClient([
+        GetGroups::class => new PowerBIFixture('groups/get-groups'),
+    ]);
+
+    $powerBIConnection = new PowerBI();
+    $authenticator = $powerBIConnection->getAccessToken();
+    $powerBIConnection->authenticate($authenticator);
+    $request = new GetGroups;
+    $response = $powerBIConnection->send($request, mockClient: $mockClient);
+    expect($response->status())->toBe(200);
+    expect($response->dto())->toBeInstanceOf(Groups::class);
+    foreach ($response->dto()->groups as $group) {
+        expect($group)->toBeInstanceOf(Group::class);
+        expect($group->id)->toBeString();
+        expect($group->isReadOnly)->toBeBool();
+        expect($group->isOnDedicatedCapacity)->toBeBool();
+        expect($group->type)->toBeString();
+        expect($group->name)->toBeString();
+    }
+});
