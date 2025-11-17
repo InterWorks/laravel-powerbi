f24ec62497c1915907615ccc1317b6d1096087da
mrkbingham <mark.bingham@interworks.com>
2025-11-17 16:46:27 -0600

    adds get reports in group call

--- src/DTO/Report.php
+++ src/DTO/Report.php
@@ -2,6 +2,7 @@
 
 namespace InterWorks\PowerBI\DTO;
 
+use InterWorks\PowerBI\Enums\ReportType;
 use Saloon\Contracts\DataObjects\WithResponse;
 use Saloon\Traits\Responses\HasResponse;
 
@@ -9,11 +10,23 @@ class Report implements WithResponse
 {
     use HasResponse;
 
+    /**
+     * @param array<int, array<string, mixed>> $users
+     * @param array<int, array<string, mixed>> $subscriptions
+     */
     public function __construct(
-        public readonly string $datasetId,
         public readonly string $id,
+        public readonly ?string $appId,
         public readonly string $name,
+        public readonly ?string $description,
+        public readonly bool $isOwnedByMe,
+        public readonly ReportType $reportType,
+        public readonly string $datasetId,
+        public readonly string $datasetWorkspaceId,
         public readonly string $webUrl,
         public readonly string $embedUrl,
+        public readonly array $users,
+        public readonly array $subscriptions,
+        public readonly int $reportFlags
     ) {}
 }
--- src/DTO/Reports.php
+++ src/DTO/Reports.php
@@ -3,6 +3,7 @@
 namespace InterWorks\PowerBI\DTO;
 
 use Illuminate\Support\Collection;
+use InterWorks\PowerBI\Enums\ReportType;
 use Saloon\Contracts\DataObjects\WithResponse;
 use Saloon\Traits\Responses\HasResponse;
 
@@ -23,22 +24,38 @@ class Reports implements WithResponse
      * Create a Reports collection from an array
      *
      * @param  array<int, array{
-     *    datasetId: string,
      *    id: string,
+     *    appId: ?string,
      *    name: string,
+     *    description: ?string,
+     *    isOwnedByMe: bool,
+     *    reportType: string,
+     *    datasetId: string,
+     *    datasetWorkspaceId: string,
      *    webUrl: string,
-     *    embedUrl: string
+     *    embedUrl: string,
+     *    users: array<int, array<string, mixed>>,
+     *    subscriptions: array<int, array<string, mixed>>,
+     *    reportFlags: int,
      * }> $data The array to parse.
      */
     public static function fromArray(array $data): self
     {
         $reports = collect($data)->map(function ($item) {
             return new Report(
-                datasetId: $item['datasetId'],
                 id: $item['id'],
+                appId: $item['appId'] ?? null,
                 name: $item['name'],
+                description: $item['description'] ?? null,
+                isOwnedByMe: $item['isOwnedByMe'],
+                reportType: ReportType::fromString($item['reportType']),
+                datasetId: $item['datasetId'],
+                datasetWorkspaceId: $item['datasetWorkspaceId'],
                 webUrl: $item['webUrl'],
                 embedUrl: $item['embedUrl'],
+                users: $item['users'],
+                subscriptions: $item['subscriptions'],
+                reportFlags: $item['reportFlags']
             );
         });
 
