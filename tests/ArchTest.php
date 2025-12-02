7e31a9f474ef9a5156df8d77f6c3f5d2dfc7e612
mrkbingham <mark.bingham@interworks.com>
2025-12-02 17:48:59 -0600

    arch test as conditional

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -1,5 +1,9 @@
 <?php
 
-arch('it will not use debugging functions')
-    ->expect(['dd', 'dump', 'ray'])
-    ->each->not->toBeUsed();
+// Architecture tests require pest-plugin-arch v2+, which requires Pest v2+
+// These tests will be skipped when using Pest v1.x (Laravel 9)
+if (function_exists('arch')) {
+    arch('it will not use debugging functions')
+        ->expect(['dd', 'dump', 'ray'])
+        ->each->not->toBeUsed();
+}
